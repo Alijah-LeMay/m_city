52494255fe4c20ca2e439e8de5e9874e49752be0
CoderBoy4715 <alijahlemay12@gmail.com>
2020-10-22 14:06:23 -0700

    changes to footer

--- src/Components/Header_footer/Footer.js
+++ src/Components/Header_footer/Footer.js
@@ -8,7 +8,13 @@ const Footer = () => {
       <div className={classes.footer_logo}>
         <CityLogo width='70px' height='70px' link={true} linkTo='/' />
       </div>
-      <div className={classes.footer_discl}>LemaTech LLC &copy; 2020</div>
+      <div className={classes.footer_discl}>
+        &copy;{' '}
+        <a className={classes.link} href='//thewebdev.net' traget='_blank'>
+          LemaTech LLC
+        </a>{' '}
+        2020
+      </div>
     </footer>
   );
 };
